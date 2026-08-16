22d9a4ba1b305f535d54d0a1516a61e5767b6d6e
Sehajdeep Singh Nibber <ssingh28_be24@thapar.edu>
2026-08-16 23:28:07 +0530

    backend api done except the addition of socket.io

--- server/src/controllers/message.controller.js
+++ server/src/controllers/message.controller.js
@@ -1,7 +1,83 @@
+import cloudinary from "../lib/cloudinary.js";
+import Message from "../models/message.model.js";
+import User from "../models/user.model.js";
+
 export const getUsersForSidebar = async (request,reply) =>{
     try {
         const loggedInUserId = request.user._id;
+        const filteredUsers = await User.find({_id: {$ne:loggedInUserId}}).select("-password")
+
+        reply.code(200).send({
+            filteredUsers
+        })
     } catch (error) {
-        
+        reply.code(500).send({
+            error: "Internal Server Error"
+        })
+    }
+}
+
+export const getMessages = async (request,reply) =>{
+    try {
+        const {id: userToChat} = request.params
+        const myId = request.user._id
+
+        const messages = await Message.find({
+            $or:[
+                {
+                    senderId: myId,
+                    receiverId: userToChat
+                },
+                {
+                    senderId: userToChat,
+                    receiverId: myId
+                },
+            ]
+        })
+
+        reply.code(200).send({
+            messages
+        })
+
+    } catch (error) {
+        reply.code(500).send({
+            error: "Internal Server Error"
+        })
+    }
+}
+
+export const sendMessage = async (request,replt) =>{
+    try {
+        const { text, image } = request.body;
+        const {id: recieverId} = request.params;
+        const senderId = request.user._id;
+
+        let imageUrl;
+
+        if(image){
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        });
+
+        await newMessage.save();
+
+        // real time functionality goes here => socket.io
+
+        reply.code(201).send({
+            newMessage
+        })
+
+    } catch (error) {
+        console.log(error.message)
+        reply.code(500).send({
+            error: "Internal Server Error"
+        })
     }
 }
--- server/src/routes/message.route.js
+++ server/src/routes/message.route.js
@@ -1,6 +1,8 @@
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getUsersForSidebar } from "../controllers/message.controller.js";
+import { getUsersForSidebar, getMessages, sendMessage } from "../controllers/message.controller.js";
 
 export default async function messageRoutes(app) {
-    app.get("/users",protectRoute,getUsersForSidebar)
+    app.get("/users",protectRoute,getUsersForSidebar);
+    app.get(":id",protectRoute,getMessages)
+    app.post("/send/:id",protectRoute,sendMessage)
 }
